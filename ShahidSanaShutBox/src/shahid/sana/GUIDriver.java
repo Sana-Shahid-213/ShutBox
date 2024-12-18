26d5772d9ca80c7e4a06edaaa4e4e5c6203c281e
Sana Shahid <sanashahid2134@gmail.com>
2024-12-18 11:44:57 -0500

    Progressed greatly on the Shut the Box game aspects

--- ShahidSanaShutBox/src/shahid/sana/GUIDriver.java
+++ ShahidSanaShutBox/src/shahid/sana/GUIDriver.java
@@ -14,84 +14,134 @@ public class GUIDriver extends Application {
 
 	Die d1 = new Die(6);
 	Die d2 = new Die(6);
-	int currentRollV;
-	boolean diceRolled= false; 
-	int inputValue = 0;
-	
+	int currentRollV = 0;
+	boolean diceRolled = false;
+	int diceRoll = 0;
+	int totalScore = 0;
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		VBox vbox = new VBox(10);
-		
+
 		// Create and display the title
 		Label title = new Label("Shut The Box");
 		vbox.getChildren().add(title);
 		HBox tileBox = new HBox(10);
 		Button[] tileBtns = new Button[9];
-		//tileBtns.set
-		Tile[] tiles = new Tile[9];
-		
-		for (int i=0; i<tileBtns.length; i++) {
-			tileBtns[i] = new Button(String.valueOf(i+1));
-			tiles[i] = new Tile(i+1);
-			tileBtns[i].setStyle("-fx-background-color:#d43e3b");
+		// tileBtns.set
+
+		for (int i = 0; i < tileBtns.length; i++) {
+			tileBtns[i] = new Button(String.valueOf(i + 1));
+			tileBtns[i].setStyle("-fx-background-color:#EEEEEE");
 			tileBox.getChildren().add(tileBtns[i]);
+
 		}
-		
+
+		for (int k = 6; k < tileBtns.length; k++) {
+
+		}
+
 		tileBox.setAlignment(Pos.CENTER);
 		vbox.getChildren().add(tileBox);
-		
+
 		Button btnRoll2 = new Button("ROLL 2 DIE");
 		Button btnRoll1 = new Button("ROLL 1 DIE");
 		Button btnTurn = new Button("Take a Turn");
 		Button btnEnd = new Button("End Turn");
-		
+		Button player2 = new Button("Switch to Player 2");
+		currentRollV = 0;
+
 		Label result = new Label("Result");
 		Label lblValue = new Label(); // output of results
-		vbox.getChildren().addAll(btnRoll2, btnRoll1, result,lblValue,btnTurn, btnEnd);
-		vbox.setAlignment(Pos.CENTER);
+		Label print = new Label("Not a valid move, Please try again");
+		Label score = new Label("Final Score");
 
-		for (Button tileBTN: tileBtns) {
-			tileBTN.setDisable(true);
-			diceRolled = false;
-		}
-		if (inputValue == 0) {
+		Label scoreVPlay1 = new Label(); // output of the score
+		print.setVisible(false);
+		vbox.getChildren().addAll(btnRoll2, btnRoll1, result, lblValue, btnTurn, btnEnd, print, score, scoreVPlay1, player2);
+		vbox.setAlignment(Pos.CENTER);
+		btnRoll2.setDisable(false);
 		btnRoll1.setDisable(true);
-		btnEnd.setDisable(true);
 		btnTurn.setDisable(true);
-		}
+		btnEnd.setDisable(true);
+		player2.setDisable(true);
 		
 		btnRoll2.setOnAction(e -> {
-			lblValue.setText(String.valueOf(d1.roll()+d2.roll()));
-			for (Button tileBTN: tileBtns) {
-				tileBTN.setDisable(false);
-				diceRolled = true;
-			}
-			
+
+			currentRollV = 0;
+			System.out.println("curren rool value when btnRoll2 press" + currentRollV);
+			diceRoll = d1.roll() + d2.roll();
+			lblValue.setText(String.valueOf(diceRoll));
+			btnTurn.setDisable(false);
+			btnRoll2.setDisable(true);
 
 		});
-		
+
 		for (Button t : tileBtns) {
 			t.setOnAction(e -> {
-		System.out.println("click");
-				t.setStyle("-fx-background-colour:#EEEEEE");
-			
+				System.out.println("click");
+				if (t.getStyle().equals("-fx-background-color:#9503a8")) {
+					t.setStyle("-fx-background-color:#EEEEEE");
+
+				} else {
+					t.setStyle("-fx-background-color:#9503a8");
+				}
+
 			});
+		}
+
+		btnTurn.setOnAction(e -> {
+			currentRollV = 0;
+			System.out.println("When the turn button is press " + currentRollV);
+
+			for (int i = 0; i < tileBtns.length; i++) {
+
+				if (tileBtns[i].getStyle().equals("-fx-background-color:#9503a8") && !(tileBtns[i].isDisabled())) {
+
+					currentRollV += i + 1;
+				}
+
 			}
-		
-		
-			//get the sytle of all the button 
-			
-			//if totale == current Rolll, disable the bottuns if its valid 
-	
-		Scene scene = new Scene(vbox,500,300);
+
+			for (Button bt : tileBtns) {
+				if (currentRollV == diceRoll) {
+					if (bt.getStyle().equals("-fx-background-color:#9503a8")) {
+
+						bt.setDisable(true);
+					}
+					btnRoll2.setDisable(false);
+				}
+			}
+
+			if (currentRollV != diceRoll) {
+				print.setVisible(true);
+				System.out.println("the value of CurrentRoll is" + currentRollV);
+				System.out.println("the value of dice roll" + diceRoll);
+			} else {
+				print.setVisible(false);
+				btnTurn.setDisable(true);
+			}
+			btnEnd.setDisable(false);
+		});
+
+		btnEnd.setOnAction(e -> {
+
+			for (int i = 0; i < tileBtns.length; i++) {
+				if (tileBtns[i].getStyle().equals("-fx-background-color:#EEEEEE")) {
+					totalScore += i + 1;
+					System.out.println(totalScore);
+					scoreVPlay1.setText(String.valueOf(totalScore));
+					btnEnd.setDisable(true);
+				}
+
+			}
+		});
+
+		Scene scene = new Scene(vbox, 500, 500);
 		stage.setScene(scene);
 		stage.show();
 	}
-	
-	
-	// for button selection, check if the first button is less than or equal to the dice rolled
-	// then after check if the first button and the second button when pressed are less than or 
-	//equal to the value of the dice roll IF GREATER THAN, DO NOT ALLOW THEM TO PREESS THE BUTTON. 
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- ShahidSanaShutBox/src/shahid/sana/GUIDriver2.java
+++ ShahidSanaShutBox/src/shahid/sana/GUIDriver2.java
@@ -18,9 +18,11 @@ public class GUIDriver2 extends Application {
 	boolean diceRolled= false;
 	boolean gameOver; 
 	int inputValue = 0;
+	int diceRoll;
 	
 	@Override
 	public void start(Stage stage) throws Exception {
+		
 		VBox vbox = new VBox(10);
 		
 		// Create and display the title
@@ -29,8 +31,15 @@ public class GUIDriver2 extends Application {
 		HBox tileBox = new HBox(10);
 		Button[] tileBtns = new Button[9];
 		//tileBtns.set
-		Tile[] tiles = new Tile[9];
-			
+	//	Tile[] tiles = new Tile[9];
+		
+		for (int i=0; i<tileBtns.length; i++) {
+			tileBtns[i] = new Button(String.valueOf(i+1));
+		//	tiles[i] = new Tile(i+1);
+			tileBtns[i].setStyle("-fx-background-color:#EEEEEE");
+			tileBox.getChildren().add(tileBtns[i]);
+		}
+		
 		tileBox.setAlignment(Pos.CENTER);
 		vbox.getChildren().add(tileBox);
 		
@@ -38,38 +47,51 @@ public class GUIDriver2 extends Application {
 		Button btnRoll1 = new Button("ROLL 1 DIE");
 		Button btnTurn = new Button("Take a Turn");
 		Button btnEnd = new Button("End Turn");
+		Button btnDone = new Button("Done");
 		
 		Label result = new Label("Result");
+		Label score = new Label("Score");
 		Label lblValue = new Label(); // output of results
-		vbox.getChildren().addAll(btnRoll2, btnRoll1, result,lblValue, btnEnd);
+		vbox.getChildren().addAll(btnRoll2, btnRoll1, result,lblValue,btnTurn, btnEnd,score);
 		vbox.setAlignment(Pos.CENTER);
-
-		for (int i=0; i<tileBtns.length; i++) {
-			tileBtns[i] = new Button(String.valueOf(i+1));
-			tiles[i] = new Tile(i+1);
-			tileBox.getChildren().add(tileBtns[i]);
+		
+		
+		for (Button tileBTN: tileBtns) {
+			tileBTN.setDisable(true);
+			diceRolled = false;
 		}
 		
+		btnRoll2.setOnAction(e -> {
+			diceRoll = d1.roll()+d2.roll();
+			lblValue.setText(String.valueOf(diceRoll));
+			for (Button tileBTN: tileBtns) {
+				tileBTN.setDisable(false);
+				btnTurn.setDisable(false);
+				diceRolled = true;
+				btnRoll2.setDisable(true);
+			}
+
+		});
+		
+		currentRollV = 0;
 		for (Button t : tileBtns) {
 			t.setOnAction(e -> {
-		
-				if(t.getStyle().equals("-fx-background-colour:#00FFFF")) {
-					t.setStyle("-fx-background-colour:#EEEEEE");
+				if(t.getStyle().equals("-fx-background-color:#00FFFF") || diceRolled) {
+					t.setStyle("-fx-background-color:#EEEEEE");
+					
 				}
 				else {
-					t.setStyle("-fx-background-colour:#00FFFF");
+					t.setStyle("-fx-background-color:#00FFFF");
 					}
 			
 			});
 			}
-			//get the sytle of all the button 
-			
-			//if totale == current Rolll, disable the bottuns if its valid 
-	
+		
 		Scene scene = new Scene(vbox,500,300);
 		stage.setScene(scene);
 		stage.show();
-	}
+		}
+
 	
 	
 	// for button selection, check if the first button is less than or equal to the dice rolled
